ab8ee5ae8f13751756bcda7a9bb615fcaae367bd
susunur <128322762+susunur@users.noreply.github.com>
2026-06-08 21:32:35 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  //React ile HTML'in köprü kurduğu yer
 
   <StrictMode>
     <BrowserRouter>
