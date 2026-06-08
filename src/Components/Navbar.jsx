d18443daaf4911d00735ba1aea917b6a3e296888
susunur <128322762+susunur@users.noreply.github.com>
2026-06-08 21:30:17 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,36 +21,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// export const Navbar = () => {
-//   return (
-//     <nav className='w-full bg-[#1e3a8a] text-white px-8 py-3 flex items-center justify-between shadow-md'>
-//         <div className="flex items-center gap-3 cursor-pointer">
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           width="32"
-//           height="32"
-//           viewBox="0 0 24 24"
-//           fill="none"
-//           stroke="currentColor"
-//           strokeWidth="2"
-//           strokeLinecap="round"
-//           strokeLinejoin="round"
-//         >
-//           <circle cx="12" cy="12" r="10"></circle>
-//           <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-//           <path d="M2 12h20"></path>
-//         </svg>
-//         <span className="text-2xl font-bold tracking-wide">D&D</span>
-//         </div>
-//         <div className="flex items-center gap-8 text-base font-medium">
-//       </div>
-
-//       <nav>
-//         <link to="/">Ana Sayfa</link>
-//         <link to="/sepetim">Sepetim</link>
-//       </nav>
-
-//     </nav>
-//   )
-// }
